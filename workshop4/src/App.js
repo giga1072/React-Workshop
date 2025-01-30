9674351990dc442a49971f2f7fd74f0b5c127846
Giga <gigachoquri2@gmail.com>
2025-01-30 14:27:26 +0400

    homework

--- workshop4/src/App.js
+++ workshop4/src/App.js
@@ -3,11 +3,26 @@ import './App.css';
 import users from './data/users';
 import UserCard from './components/UserCard';
 import { useState } from 'react';
+import Login from './components/loggedIn/logIn';
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  function handleLogin(credentials) {
+    const { username, password } = credentials;
+    if (username === "digitalAcademy@gmail.com" && password === "iLoveReact123") {
+      setIsAuthenticated(true);
+    } else {
+      alert("Incorrect username or password!");
+    }
+  }
   const [visibleCount, setVisibleCount] = useState(5);
   const showMoreCards = () => {
     setVisibleCount(visibleCount + 5);
   };
+  if (!isAuthenticated){
+    return < Login onLogin={handleLogin}/>
+  };
+  
   return (
     <div className="App">
       <div className="user-card-container">
