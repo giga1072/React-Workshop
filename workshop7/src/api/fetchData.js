dea2a243f3ff734bda22c0be51a6ecee646239f3
giga1072 <gigachoquri@gmail.com>
2025-02-10 13:42:05 +0400

    Delete workshop7 directory

--- workshop7/src/api/fetchData.js
+++
@@ -1,7 +0,0 @@
-export async function fetchData(url) {
-    const response = await fetch(url);
-    if (response.ok) {
-        return response.json();
-    }
-    throw new Error("Request failed!");
-}
